0032716ed315f39f33068dcd53e14b32d7da2fce
Yaika <yaikazeeman@hotmail.com>
2022-02-01 18:20:56 +0100

    bla bla

--- sessions/session3/exercises/bands.js
+++ sessions/session3/exercises/bands.js
@@ -25,3 +25,30 @@ const bands = [
 // Each band should be inside a div
 // Band name should be in a h2 tag
 // Band members should be in a p tag each with its instrument as class attribute
+
+const main = document.querySelector('.main');
+
+for (let i in bands){
+  let band = bands[i];
+  let bandDiv = document.createElement("div");
+
+  bandDiv.innerHTML = "<h2>" + band.name + "</h2>";
+
+  for (let member in band.instruments){
+    let memberContainer = document.createElement("p");
+    memberContainer.innerHTML = member;
+    memberContainer.classList.add(band.instruments[member]);
+    bandDiv.appendChild(memberContainer)
+  }
+
+  main.appendChild(bandDiv);
+}
+
+
+const show = () => {
+  const myContainer = document.createElement("div");
+  myContainer.innerHTML = '<h2>' + HI + '</h2>'
+  document.appendChild(myContainer)
+}
+
+button.addEventListener('click', show)
--- sessions/session3/exercises/events.js
+++ sessions/session3/exercises/events.js
@@ -2,6 +2,17 @@
 
 // add a handler to button 1 so it changes the background of the page when clicked
 
+
+const button1 = document.querySelector("button1");
+const body = document.querySelector("body");
+
+const button1Handler = () => {
+    body.style.backgroundColor = "red";
+    console.log("hi");
+}
+
+button1.addEventListener("click", button1Handler)
+
 // add a handler to button 2 so it adds the text "dynamically created" to the <h1>
 
 // add a handler to button 3 so it changes the color of the text in the title
