d6396cae9a2bdb6523e1742f3b51a9fcf3cb343f
NickDeitmers <71077922+NickDeitmers@users.noreply.github.com>
2022-02-15 17:42:07 +0100

    add homework users.js

--- sessions/session5/exercises/users.js
+++ sessions/session5/exercises/users.js
@@ -1,3 +1,5 @@
+// By Raban Sieglier, Jules de Vil, Nick Deitmers
+
 // <---------User creation form code----------->
 
 function createUser(event) {
@@ -12,7 +14,9 @@ function createUser(event) {
         body: JSON.stringify(getUserData())
     };
 
-    // YOUR FETCH CODE HERE
+    fetch(fetchUrl, fetchOptions)
+        .then(response => response.json())
+        .then(data => console.log(data));
 }
 
 function getUserData() {
@@ -26,38 +30,47 @@ function getUserData() {
 }
 
 function getUserName() {
-    const userNameInput = document.querySelector('#userName');
+    const userNameInput = document.getElementById('userName');
     return userNameInput.value;
 }
 
 function getEmail() {
-    // YOUR CODE HERE
+    const emailInput = document.getElementById('email');
+    return emailInput.value;
 }
 
 function getFavouriteBand() {
-    // YOUR CODE HERE
+    const bandInput = document.getElementById('favouriteBand');
+    return bandInput.value;
 }
 
 function getFavouriteDish() {
-    // YOUR CODE HERE
+    const dishInput = document.getElementById('favouriteDish');
+    return dishInput.value;
 }
 
 function getSomethingAboutYou() {
-    // YOUR CODE HERE
+    const aboutInput = document.getElementById('somethingAboutYou');
+    return aboutInput.value;
 }
 
 // Use document.querySelector and addEventListener to select the form in the HTML (you can do it via the tag selector or via the id selector) and add a listener to the "submit" event with the eventHandler "createUser"
 
-// YOUR CODE HERE FOR EVENT HANDLING
+const forum = document.querySelector('#create-user-form')
+forum.addEventListener('submit', createUser)
 
 // <---------End of user creation form code----------->
 
 // <---------Refresh user list code----------->
 
 function refreshUserList() {
-    const fetchUrl = 'https://mscbt-luis-grande.herokuapp.com/session5/users';
+    const fetchUrl = 'https://mscbt-luis-grande.herokuapp.com/session5/users'
+    
 
-    // YOUR FETCH CODE HERE
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(data => {renderUserList(data)});
+
 }
 
 function renderUserList(users) {
@@ -74,8 +87,8 @@ function renderUserList(users) {
         // Add text list element
         listElement.innerText = user.userName;
 
-        // YOUR CODE FOR EVENT HANDLING HERE
-
+        listElement.addEventListener('click', () => {getUserInfo(user.userName)})
+        
         // Append list element to userList element
         userList.appendChild(listElement);
     });
@@ -88,93 +101,47 @@ document.getElementById('refresh-users-button').addEventListener('click', refres
 // <---------Get user info code----------->
 
 function getUserInfo(userName) {
-    const fetchUrl = 'https://mscbt-luis-grande.herokuapp.com/session5/users/' + userName;
+    const fetchUrl = 'https://mscbt-luis-grande.herokuapp.com/session5/users' + userName;
 
-    // YOUR FETCH CODE HERE
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            renderUserList(data)});
+        
 }
 
 function renderUserInfo(userData) {
     // Select HTML element with "userInfo" id
-    // YOUR CODE HERE
+    const userInfo = document.getElementById("userInfo")
 
     // Set innerHTML property of selected element to an empty string
-    // YOUR CODE HERE
+    userInfo.innerHTML = '';
 
     // Loop over the userData object (Remember, you can use the for...in loop style we saw in session3)
     for (userKey in userData) {
         // Create an h3 element
-        // YOUR CODE HERE
+        const h3_element = document.createElement('h3');
 
         // Set the innerText property of the h3 element to the key of the loop interation
-        // YOUR CODE HERE
+        h3_element.innerText = userKey;
 
         // Create a p element
-        // YOUR CODE HERE
+        const p_element = document.createElement('p');
 
         // Set the innerText property of the p element to the value of the loop iteration
-        // YOUR CODE HERE
+        p_element.innerText = userData[userKey];
 
         // Append the h3 element to the userInfo container
-        // YOUR CODE HERE
+        user_info.appendChild(h3_element);
 
         // Append the p element to the userInfo container
-        // YOUR CODE HERE
+        user_info.appendChild(p_element);
 
     }
 }
 
 // <---------End of get user info code----------->
-
-
-// <---------Part 1----------->
-
-// Fill in the code for getEmail, getFavouriteBand, getFavouriteDish and getSomethingAboutYou functions to get the values of the inputs in the form (you can use getUserName as an example)
-
-// Fill in the code for createUser function to use fetch to call the "fetchUrl" with the provided "fetchOptions".
-
-// Fill in the code where it says YOUR CODE HERE FOR EVENT HANDLING
-
-// In the web browser, fill in the data in the form to tell us something about you and click the create user button
-
-// <--------- End of Part 1----------->
-
-// <---------Part 2----------->
-
-// Fill in the code refreshUserList function to use fetch to call the "fetchUrl", extract the json from the response and render a list of users (Hint: there is a function already written for that)
-
-// Click the refresh user list button and see what happens. We will discus what's happened
-
-// Modify the function that renders the user list and add an event handler to each list element that when the elements gets clicked, executes the getUserInfo handler passing the user as a parameter of the function call
-
-// <--------- End of Part 2----------->
-
-// <---------Part 3----------->
-
-// Fill in the code for getUserInfo function to use fetch to cal the "fetchUrl", extract the json from the response and render the info for that user
-
-// Fill in the code of renderUserInfo to actually render the info for the user we just fetched from the web server.
-
-// Now, when clicking on one of the users in the user list you should get a detail of the data for that user
-
-// <--------- End of Part 3----------->
-
-// <---------Homework----------->
-
-// Feel free to join in groups to do the homework (Top 3/4 people). If that's the case, put the group members in the top of the file as a code comment
-// Please push a branch to the repo with your name/s as branch name with the solutions for homework
-
-// In Part 1, show a success notification when receiving the response from the server when creating a new user
-// Hint: there are already some functions implemented to show notifications (Check the bottom of the file)
-// In Part 1, create an if else clause that executes two different things. If the json we get from the response contains a key "errorMessage", then show an error notification and if not, show the success notification we just created.
-// Note: The web server will return this "errorMessage" key in the response when trying to create a user who has a userName that has already been used.
-
-// Do Part 3 of the exercise
-
-// If you want to prepare for next class, here you have a couple of CodeAcademy links that will be very useful:
-// https://www.codecademy.com/courses/learn-css/lessons/box-model-intro/exercises/box-model-intro
-// https://www.codecademy.com/courses/learn-css/lessons/css-display-positioning
-
-// <---------End of homework----------->
 
 // <---------Notification code----------->
 function showUserCreatedNotification(userName) {
